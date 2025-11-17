0b6926c37dea27055112513ee05657e2f61b0cd8
Diego Garcia <zackmododiablo@gmail.com>
2025-11-17 00:52:39 -0500

    Change the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className="max-w-4xl mx-auto flex flex-col items-center space-y-6 text-center">
 
         <p className="text-xs text-zinc-500">
-          © {new Date().getFullYear()} Los Flechados — Todos los derechos reservados.
+          © {new Date().getFullYear()} Los Falsos — Todos los derechos reservados.
         </p>
       </div>
     </footer>
